3402da9757e6f85dbb1a68e65ed68d22ee3a4727
MuKassab <muhammadkassab97@gmail.com>
2024-02-15 03:09:42 +0200

    change db folder

--- src/db.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-import { config } from './common/config/index.js';
-
-const sequelize = new Sequelize(config.POSTGRES_URI);
-
-// Return sequelize instance of the connection to the database
-export default sequelize;
